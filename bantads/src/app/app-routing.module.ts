f2d821eb7b4e873f141312f5133d740ec0d9837d
Luis Augusto Ferraz de Souza <luisprto@gmail.com>
2022-12-13 23:31:22 -0300

    cliente routes e app routes corrigido

--- bantads/src/app/app-routing.module.ts
+++ bantads/src/app/app-routing.module.ts
@@ -2,70 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminRoutes } from './admin/admin-routing.module';
 import { LoginRoutes } from './auth/auth-routing.module';
+import { ClienteRoutes } from './cliente/cliente-routing.module';
 
 
 const routes: Routes = [
   {
-    path : '',
+    path: '',
     redirectTo: 'login',
     pathMatch: 'full',
   },
-  {
-    path: 'admin',
-    redirectTo: 'admin/inicio'
-  },
-  {
-    path: 'admin/inicio',
-    component: TelaInicialComponent
-  },
-  {
-    path: 'admin/listar-gerente',
-    component : ListarGerentesComponent
-  },
-  {
-    path: 'admin/editar-gerente/:id',
-    component: EditarGerenteComponent
-  },
-  {
-    path: 'admin/novo-gerente',
-    component: InserirGerenteComponent
-  },
-  {
-    path:'admin/relatorio-clientes',
-    component: RelatorioClientesComponent
-  },
-  {
-    path:'cliente/autocadastro',
-    component: AutocadastroComponent
-  },
-  {
-    path:'cliente/consultar-extrato',
-    component: ConsultarExtratoComponent
-  },
-  {
-    path:'cliente/consultar-saldo',
-    component: ConsultarSaldoComponent
-  },
-  {
-    path:'cliente/depositar',
-    component: DespositarComponent
-  },
-  {
-    path:'cliente/editar-perfil',
-    component: EditarPerfilComponent
-  },
-  {
-    path:'cliente/sacar',
-    component: SacarComponent
-  },
-  {
-    path:'cliente/inicial',
-    component: TelaInicialClienteComponent
-  },
-  {
-    path:'cliente,',
-    redirectTo: 'cliente/inicial'
-  }
+  ...AdminRoutes,
+  ...LoginRoutes,
+  ...ClienteRoutes
 ];
 
 @NgModule({
---
+++ bantads/src/app/cliente/cliente-routing.module.ts
@@ -0,0 +1,43 @@
+import { Routes } from "@angular/router";
+import { AutocadastroComponent } from "./autocadastro/autocadastro.component";
+import { ConsultarExtratoComponent } from "./consultar-extrato/consultar-extrato.component";
+import { ConsultarSaldoComponent } from "./consultar-saldo/consultar-saldo.component";
+import { DespositarComponent } from "./despositar/despositar.component";
+import { EditarPerfilComponent } from "./editar-perfil/editar-perfil.component";
+import { SacarComponent } from "./sacar/sacar.component";
+import { TelaInicialClienteComponent } from "./tela-inicial-cliente/tela-inicial-cliente.component";
+
+export const ClienteRoutes: Routes = [
+
+  {
+    path: 'cliente/autocadastro',
+    component: AutocadastroComponent
+  },
+  {
+    path: 'cliente/consultar-extrato',
+    component: ConsultarExtratoComponent
+  },
+  {
+    path: 'cliente/consultar-saldo',
+    component: ConsultarSaldoComponent
+  },
+  {
+    path: 'cliente/depositar',
+    component: DespositarComponent
+  },
+  {
+    path: 'cliente/editar-perfil',
+    component: EditarPerfilComponent
+  },
+  {
+    path: 'cliente/sacar',
+    component: SacarComponent
+  },
+  {
+    path: 'cliente/inicial',
+    component: TelaInicialClienteComponent
+  },
+  {
+    path: 'cliente,',
+    redirectTo: 'cliente/inicial'
+  }];
