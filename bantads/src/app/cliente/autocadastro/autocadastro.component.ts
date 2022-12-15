ffd09d088b4d9c129828d99eda9d86638dee11e4
Francielle Mazzarotto Stival <franciellestival@gmail.com>
2022-12-15 18:57:49 -0300

    layout++ cliente module

--- bantads/src/app/cliente/autocadastro/autocadastro.component.ts
+++ bantads/src/app/cliente/autocadastro/autocadastro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Cliente } from 'src/app/shared';
+import { ClienteService } from '../services';
 
 @Component({
   selector: 'app-autocadastro',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutocadastroComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formCliente') formCliente! : NgForm;
+  cliente! : Cliente;
+
+  constructor(private clienteService: ClienteService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.cliente = new Cliente();
+  }
+
+  inserir() : void{
+    if(this.formCliente.form.valid){
+      this.clienteService.inserir(this.cliente);
+      this.router.navigate(['/cliente']);
+    }
   }
 
 }
