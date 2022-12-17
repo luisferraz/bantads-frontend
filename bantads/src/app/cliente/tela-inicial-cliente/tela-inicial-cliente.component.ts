c09cfd80305c8e54ff5c514c066df9dd71a542a4
Guilherme Janke <guilhermejanke15@gmail.com>
2022-12-17 16:06:29 -0300

    implementação de métodos do cliente

--- bantads/src/app/cliente/tela-inicial-cliente/tela-inicial-cliente.component.ts
+++ bantads/src/app/cliente/tela-inicial-cliente/tela-inicial-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import SaldoResponse, { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-tela-inicial-cliente',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaInicialClienteComponent implements OnInit {
 
-  constructor() { }
+  saldo?: SaldoResponse;
+
+  constructor(private clienteService: ClienteService, private route: ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    
   }
 
+  getSaldo(): void {
+
+    let id = +this.route.snapshot.params['id'];
+    this.clienteService.getSaldo(id).subscribe((res) => {
+      if (res) {
+        console.log(res);
+        this.saldo = res;
+      }
+      else {
+        throw new Error('Falha ao carregar o saldo do cliente = ' + id);
+      }
+    });
+  }
 }
